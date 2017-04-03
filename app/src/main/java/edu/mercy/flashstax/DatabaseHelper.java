267f379cc6a9f2b59a5849fd349e2ca740fd8291
Stephen Nilsen <stevenilsen@live.com>
2017-04-02 22:50:20 -0400

    Added DatabaseHelper java file and loaded all the constants from our SRS that will be used to implement the database.

---
+++ app/src/main/java/edu/mercy/flashstax/DatabaseHelper.java
@@ -0,0 +1,81 @@
+package edu.mercy.flashstax;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "FlashStax.db";
+    public static final int DATABASE_VER = 1;
+
+    // cards table
+    public static final String TABLE_CARDS = "cards";
+    public static final String COL_CARD_STACK_NAME = "stackName";
+    public static final String COL_CARD_INDEX = "index";
+    public static final String COL_CARD_FRONT_TEXT = "frontText";
+    public static final String COL_CARD_BACK_TEXT = "backText";
+    public static final String COL_CARD_ACTIVE_FLAG = "activeFlag";
+    public static final String COL_CARD_DATE_TIME_CR = "dateTimeCR";
+    public static final String COL_CARD_DATE_TIME_LM = "dateTimeLM";
+
+    public static final String SQL_CREATE_TABLE_CARDS = "CREATE TABLE " + TABLE_CARDS + "("
+            + COL_CARD_STACK_NAME + " VARCHAR(50),"
+            + COL_CARD_INDEX + " INTEGER,"
+            + COL_CARD_FRONT_TEXT + " VARCHAR(500),"
+            + COL_CARD_BACK_TEXT + " VARCHAR(500),"
+            + COL_CARD_ACTIVE_FLAG + " BOOLEAN,"
+            + COL_CARD_DATE_TIME_CR + " TIMESTAMP,"
+            + COL_CARD_DATE_TIME_LM + " TIMESTAMP"
+            + ");";
+
+    // stacks table
+    public static final String TABLE_STACKS = "stacks";
+    public static final String COL_STACK_NAME = "name";
+    public static final String COL_STACK_CATEGORY = "category";
+    public static final String COL_STACK_INDEX = "index";
+    public static final String COL_STACK_ACTIVE_FLAG = "activeFlag";
+    public static final String COL_STACK_DATE_TIME_CR = "dateTimeCR";
+    public static final String COL_STACK_DATE_TIME_LM = "dateTimeLM";
+
+    public static final String SQL_CREATE_TABLE_STACKS = "CREATE TABLE " + TABLE_STACKS + "("
+            + COL_STACK_NAME + " VARCHAR(50),"
+            + COL_STACK_CATEGORY + " VARCHAR(50),"
+            + COL_STACK_INDEX + " INTEGER,"
+            + COL_STACK_ACTIVE_FLAG + " BOOLEAN,"
+            + COL_STACK_DATE_TIME_CR + " TIMESTAMP,"
+            + COL_STACK_DATE_TIME_LM + " TIMESTAMP"
+            + ");";
+
+    // categories table
+    public static final String TABLE_CATEGORIES = "categories";
+    public static final String COL_CAT_NAME = "name";
+    public static final String COL_CAT_COLOR = "color";
+    public static final String COL_CAT_INDEX = "index";
+    public static final String COL_CAT_ACTIVE_FLAG = "activeFlag";
+    public static final String COL_CAT_DATE_TIME_CR = "dateTimeCR";
+    public static final String COL_CAT_DATE_TIME_LM = "dateTimeLM";
+
+    public static final String SQL_CREATE_TABLE_CATEGORIES = "CREATE TABLE " + TABLE_CATEGORIES + "("
+            + COL_CAT_NAME + " VARCHAR(50),"
+            + COL_CAT_COLOR + " INTEGER,"
+            + COL_CAT_INDEX + " INTEGER,"
+            + COL_CAT_ACTIVE_FLAG + " BOOLEAN,"
+            + COL_CAT_DATE_TIME_CR + " TIMESTAMP,"
+            + COL_CAT_DATE_TIME_LM + " TIMESTAMP"
+            + ");";
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
